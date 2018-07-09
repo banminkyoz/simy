3026d381f9008e80bfc624fe5c285c993d584b92
Kyoz <banminkyoz@gmail.com>
2018-07-09 11:54:37 +0700

    Update test

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import sincewemet from '.';
+import simy from '.';
 
 test('get correct simy times', t => {
-  t.deepEqual(sincewemet.getFirstTimeMet(), '11/06/2017 08:23:00');
-  t.truthy(sincewemet.getSinceIMetYou());
-  t.truthy(sincewemet.getSinceIMetYouDetail());
+  t.deepEqual(simy.getFirstTimeMet(), '11/06/2017 08:23:00');
+  t.truthy(simy.getSinceIMetYou());
+  t.truthy(simy.getSinceIMetYouDetail());
 });
